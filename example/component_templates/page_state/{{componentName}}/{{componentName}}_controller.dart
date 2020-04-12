028d08c71ec450a5480c3435353859902a949b62
Masayuki Ono <mono0926@gmail.com>
2020-04-12 11:07:50 +0900

    Add provider static method to controller

--- example/component_templates/page_state/{{componentName}}/{{componentName}}_controller.dart
+++ example/component_templates/page_state/{{componentName}}/{{componentName}}_controller.dart
@@ -1,3 +1,6 @@
+import 'package:flutter/widgets.dart';
+import 'package:flutter_state_notifier/flutter_state_notifier.dart';
+import 'package:provider/single_child_widget.dart';
 import 'package:state_notifier/state_notifier.dart';
 
 import '{{componentName}}_state.dart';
@@ -6,4 +9,13 @@ export '{{componentName}}_state.dart';
 
 class {{className}}Controller extends StateNotifier<{{className}}State> with LocatorMixin {
   {{className}}Controller() : super(const {{className}}State());
+
+  static SingleChildWidget provider({
+    Widget child,
+  }) {
+    return StateNotifierProvider<MonoPageController, MonoPageState>(
+      create: (context) => MonoPageController(),
+      child: child,
+    );
+  }
 }
