f53a5ffaff664e8b36dc40c4493caf85e3bd60f3
Masayuki Ono <mono0926@gmail.com>
2020-04-12 11:13:31 +0900

    Add wrapped static method to page

--- example/component_templates/page_state/{{componentName}}/{{componentName}}.dart
+++ example/component_templates/page_state/{{componentName}}/{{componentName}}.dart
@@ -4,13 +4,22 @@ import 'package:provider/provider.dart';
 import '{{componentName}}_controller.dart';
 
 class {{className}} extends StatelessWidget {
-  const {{className}}({Key key}) : super(key: key);
+  const {{className}}._({Key key}) : super(key: key);
 
   static const routeName = '/{{componentName}}';
 
+  static Widget wrapped() {
+    return MultiProvider(
+      providers: [
+        {{className}}Controller.provider(),
+      ],
+      child: const {{className}}._(),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final controller = context.watch<MonoPageController>();
+    final controller = context.watch<{{className}}Controller>();
     return Scaffold(
       appBar: AppBar(
         title: Text(
